557a560dc7de16bdc7d7407c2100fb30841a09fb
Ali Saadati <saadati944@gmail.com>
2021-04-11 15:49:19 +0430

    add a article view and add routes "/article/<int:article_id>"

--- djblogger/articles/urls.py
+++ djblogger/articles/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+from .views import article
+
+app_name = 'articles'
+
 urlpatterns = [
-    
+    path('<int:article_id>/', article, name='article')
 ]
--- djblogger/articles/views.py
+++ djblogger/articles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def article(request, article_id):
+    return HttpResponse("you have requested article number "+str(article_id))
+
+def create_article(request):
+    pass
