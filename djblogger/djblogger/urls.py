af669c89531119d66fe2cc46bb62203c92f9d82e
Ali Saadati <saadati944@gmail.com>
2021-04-12 18:57:35 +0430

    add ckeditor to urls.py

--- djblogger/djblogger/urls.py
+++ djblogger/djblogger/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('article/', include('articles.urls'))
+    path('article/', include('articles.urls')),
+    url('^ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 # serve /media/ during development.
