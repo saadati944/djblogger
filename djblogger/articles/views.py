4d6bf600934ebadeb8cd8361bbd0670ef62f6317
Ali Saadati <saadati944@gmail.com>
2021-04-13 09:30:08 +0430

    view an article with its id route : /article/<int:article_id>

--- djblogger/articles/views.py
+++ djblogger/articles/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article, Author
 
 
 def article(request, article_id):
-    return HttpResponse("you have requested article number "+str(article_id))
+    art = None
+    art = Article.objects.filter(id = article_id).first()
+    if art == None :
+        # tdoo : 
+        return render(request, "default/articlenotfound.html", {
+            'id':article_id
+        })
+    return render(request, 'default/viewer.html', {
+        'title' : art.title,
+        'author' : art.author,
+        'date_created' : art.date_created,
+        'content' : art.content
+    })
 
 def create_article(request):
     pass
